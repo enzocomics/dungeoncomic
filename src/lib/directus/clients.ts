0fee8b0b6c4ca9c68029940e271f4b497795a417
Enzo <enzo@cheerupemokid.com>
2026-07-10 09:17:34 -0700

    set up directus sdk clients

---
+++ src/lib/directus/clients.ts
@@ -0,0 +1,31 @@
+/** ------------------------------------------------ **/
+// LIBRARIES
+import {
+	authentication,
+	createDirectus,
+	rest,
+	staticToken,
+} from "@directus/sdk"
+
+// VARIABLES
+import { adminToken as adminToken, directusURL } from "@/lib/env"
+
+// CMS
+import { DirectusSchema } from "@/lib/directus/schema"
+
+/** ------------------------------------------------ **/
+// Directus client for making public API queries
+export const publicClient =
+	createDirectus<DirectusSchema>(directusURL).with(rest())
+
+/** ------------------------------------------------ **/
+// Directus client for making user API queries
+export const userClient = createDirectus<DirectusSchema>(directusURL)
+	.with(authentication())
+	.with(rest())
+
+/** ------------------------------------------------ **/
+// Directus client for making admin API queries
+export const adminClient = createDirectus<DirectusSchema>(directusURL)
+	.with(staticToken(adminToken))
+	.with(rest())
---
+++ src/lib/env.ts
@@ -0,0 +1,3 @@
+// Environment Variables
+export const directusURL = process.env.NEXT_PUBLIC_CMS_URL!
+export const adminToken = process.env.CMS_ADMIN_TOKEN!
