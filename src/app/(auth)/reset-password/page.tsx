934a7e0b52bc937d1afc91bcf7a6299908bd5a5e
Enzo <enzo@cheerupemokid.com>
2026-07-14 15:33:44 -0700

    useSearchParams() requires suspense boundary

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,14 +1,22 @@
 /**----------------------------------- */
 // LIBRARIES
 import { Metadata } from "next"
+import { Suspense } from "react"
+// I18N
 import { useTranslations } from "next-intl"
 import { getTranslations } from "next-intl/server"
+// UI
 import ResetPasswordPageUI from "./_ui"
 
 /**----------------------------------- */
 export default function ResetPasswordPage() {
+	// I18N
 	const t = useTranslations("auth")
-	return <ResetPasswordPageUI />
+	// OUTPUT
+	// - The suspense boundary is required because we call useSearchParams() in the UI
+	return <Suspense>
+		<ResetPasswordPageUI />
+	</Suspense>
 }
 /** ------------------------------------------------ **
  * Page Metadata
