7f40a3d258b34c80f086be3916faf1c1468ac809
Enzo <enzo@cheerupemokid.com>
2026-07-19 22:30:14 -0700

    clarify comments

--- src/app/(auth)/logout/_action.ts
+++ src/app/(auth)/logout/_action.ts
@@ -1,19 +1,24 @@
 "use server"
-import { deleteUserCookie, getUserCookie } from "@/data/cookies"
-import { userCookieName } from "@/data/env"
-import { userClient } from "@/lib/directus/clients"
+/**----------------------------------- */
+// LIBRARIES
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { userClient } from "@/lib/directus/clients"
+import { deleteUserCookie, getUserCookie } from "@/data/cookies"
 
+/** ------------------------------------------------ **
+ * LOGOUT ACTION
+ */
 export async function logout() {
 	const cookieStore = await cookies()
 	const cookie = await getUserCookie()
 	if (cookie) {
 		const refresh_token = cookie.refresh_token as string
+		// Log out of Directus
 		await userClient.logout({ refresh_token: refresh_token })
-
-		cookieStore.delete(userCookieName)
-		// await deleteUserCookie(cookieStore)
-		redirect("/")
+		// Delete the cookie
+		await deleteUserCookie(cookieStore)
+		// Redirect to homepage
 	}
+	redirect("/")
 }
