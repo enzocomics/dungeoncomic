4fc57fa54cf7cbb65d2c950b6174085c24ab656e
Enzo <enzo@cheerupemokid.com>
2026-07-19 17:15:06 -0700

    add session secret env

---
+++ src/data/env.ts
@@ -0,0 +1,4 @@
+// Environment Variables
+export const directusURL = process.env.NEXT_PUBLIC_CMS_URL!
+export const adminToken = process.env.CMS_ADMIN_TOKEN!
+export const sessionSecret = process.env.CMS_SESSION_SECRET!
