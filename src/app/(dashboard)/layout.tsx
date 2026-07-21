88c73498eb9bfa797433175c3bd9587ef5915169
Enzo <enzo@cheerupemokid.com>
2026-07-21 10:27:46 -0700

    dashboard has its own root layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+"use server"
+/**----------------------------------- */
+import "@/styles/globals.css"
+// FUNCTIONS
+import clsx from "clsx"
+import { NextIntlClientProvider } from "next-intl"
+
+export default async function DashboardRootLayoutUI(props: LayoutProps<"/">) {
+	return <html lang="en">
+		<body
+			className={clsx(
+			)}
+		>
+			<NextIntlClientProvider>
+				{props.children}
+			</NextIntlClientProvider>
+		</body>
+	</html>
+}
