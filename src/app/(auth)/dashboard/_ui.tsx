301d78580d5ba10f08b30ab48b21e96a0b42811f
Enzo <enzo@cheerupemokid.com>
2026-07-21 10:23:05 -0700

    clarify comments

--- src/app/(auth)/dashboard/_ui.tsx
+++ src/app/(auth)/dashboard/_ui.tsx
@@ -1,13 +1,16 @@
+/**----------------------------------- */
+// I18N
 import { useTranslations } from "next-intl"
+// LIBRARIES
 import Link from "next/link"
-import { logout } from "../logout/_action"
 
+/**-----------------------------------
+ * Dasboard Page UI
+ */
 export default function DashboardPageUI() {
 	const t = useTranslations("auth")
 	return <>
 		<h1 className="text-3xl">{t("pages.dashboard.title")}</h1>
-
 		<Link href="/logout">Logout</Link>
-
 	</>
 }
