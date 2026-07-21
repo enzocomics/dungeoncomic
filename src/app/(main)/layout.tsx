e4975388fbf0fb2729adc57f4119f69660bd77e9
Enzo <enzo@cheerupemokid.com>
2026-07-21 10:19:55 -0700

    add toast container to main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,8 +3,13 @@
 import "@/styles/globals.css"
 // FUNCTIONS
 import clsx from "clsx"
+// LIBRARIES
 import { NextIntlClientProvider } from "next-intl"
+import { ToastContainer } from "react-toastify"
 
+/**-----------------------------------
+ * ROOT LAYOUT - MAIN APP
+ */
 export default async function MainRootLayoutUI(props: LayoutProps<"/">) {
 	return <html lang="en">
 		<body
@@ -13,6 +18,7 @@ export default async function MainRootLayoutUI(props: LayoutProps<"/">) {
 		>
 			<NextIntlClientProvider>
 				{props.children}
+				<ToastContainer />
 			</NextIntlClientProvider>
 		</body>
 	</html>
