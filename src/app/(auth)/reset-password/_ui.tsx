ed0acc1740b6477f63784fad55490f49b3c79e7b
Enzo <enzo@cheerupemokid.com>
2026-07-29 13:44:20 -0700

    add authlayout to reset-password

--- src/app/(auth)/reset-password/_ui.tsx
+++ src/app/(auth)/reset-password/_ui.tsx
@@ -1,5 +1,7 @@
 "use client"
 /**----------------------------------- */
+// FUNCTIONS
+import clsx from "clsx"
 // NEXT
 import { redirect, useSearchParams } from "next/navigation"
 // I18N
@@ -12,6 +14,10 @@ import { resetPasswordRequestSchema, resetPasswordSubmitSchema } from "@/lib/zod
 import { requestReset, submitReset } from "./_action"
 // UI
 import { useChangeStatus } from "@/components/status-message"
+import { AuthBody, AuthHeader, AuthLayout } from "@/components/auth-layout"
+import { Fieldset, Label } from "@/components/fieldset"
+import { Button } from "@/components/button"
+import { Input } from "@/components/input"
 
 /** ------------------------------------------------ **
  * RESET PASSWORD PAGE
@@ -21,7 +27,11 @@ export default function ResetPasswordPageUI() {
 	const params = useSearchParams()
 	const resetToken = params.get("token")
 	// OUTPUT
-	return resetToken ? <ResetPasswordForm token={resetToken} /> : <RequestResetForm />
+	return <>
+		<AuthLayout>
+			{resetToken ? <ResetPasswordForm token={resetToken} /> : <RequestResetForm />}
+		</AuthLayout>
+	</>
 }
 
 /** ------------------------------------------------ **
@@ -58,29 +68,42 @@ function RequestResetForm() {
 
 	// OUTPUT
 	return <>
-		<h1 className="text-3xl">{t("pages.reset-password.title")}</h1>
-		<form
-			id={form.id}
-			onSubmit={form.onSubmit}
-			action={action}
-			noValidate
-		>
-			<div>
-				<label htmlFor="email">{t("fields.email")}</label>
-				<input
-					id="email"
-					type="email"
-					key={fields.email.key}
-					name={fields.email.name}
-					// Retain the value of the previous submission
-					defaultValue={lastResult?.initialValue?.email as string}
-				/>
-				<div>{fields.email.errors}</div>
-			</div>
-			<button
-				type="submit"
-			>{t("pages.reset-password.request")}</button>
-		</form>
+		<AuthHeader>
+			{t("pages.reset-password.title")}
+		</AuthHeader>
+		<AuthBody>
+			<form
+				id={form.id}
+				onSubmit={form.onSubmit}
+				action={action}
+				noValidate
+			>
+				<Fieldset
+					className={clsx(
+						"space-y-6"
+					)}>
+					<div>
+						<label htmlFor={fields.email.name}>{t("fields.email")}</label>
+						<Input
+							id={fields.email.name}
+							type="email"
+							key={fields.email.key}
+							name={fields.email.name}
+							// Retain the value of the previous submission
+							defaultValue={lastResult?.initialValue?.email as string}
+						/>
+						<Label htmlFor={fields.email.name} className="font-normal text-red-500">{fields.email.errors}</Label>
+					</div>
+					<Button
+						type="submit"
+						color="sky"
+						className={clsx(
+							"w-full",
+						)}
+					>{t("pages.reset-password.request")}</Button>
+				</Fieldset>
+			</form>
+		</AuthBody>
 	</>
 }
 
@@ -119,43 +142,54 @@ function ResetPasswordForm({ token }: { token: string }) {
 
 	// OUTPUT
 	return <>
-		<h1 className="text-3xl">{t("pages.reset-password.title")}</h1>
-		<form
-			id={form.id}
-			onSubmit={form.onSubmit}
-			action={action}
-			noValidate
-		>
-			<div>
-				<label htmlFor="password">{t("fields.new-password")}</label>
-				<input
-					id="password"
-					type="password"
-					key={fields.password.key}
-					name={fields.password.name}
-					// Retain the value of the previous submission
-					defaultValue={lastResult?.initialValue?.password as string}
-				/>
-				<div>{fields.password.errors}</div>
-			</div>
+		<AuthHeader>{t("pages.reset-password.title")}</AuthHeader>
+		<AuthBody>
+			<form
+				id={form.id}
+				onSubmit={form.onSubmit}
+				action={action}
+				noValidate
+			>
+				<Fieldset
+					className={clsx(
+						"space-y-6"
+					)}>
+					<div>
+						<Label htmlFor={fields.password.name}>{t("fields.new-password")}</Label>
+						<Input
+							id={fields.password.name}
+							type="password"
+							key={fields.password.key}
+							name={fields.password.name}
+							// Retain the value of the previous submission
+							defaultValue={lastResult?.initialValue?.password as string}
+						/>
+						<Label htmlFor={fields.password.name} className="font-normal text-red-500">{fields.password.errors}</Label>
+					</div>
 
-			<div>
-				<label htmlFor="passwordConfirm">{t("fields.new-password-confirm")}</label>
-				<input
-					id="passwordConfirm"
-					type="password"
-					key={fields.passwordConfirm.key}
-					name={fields.passwordConfirm.name}
-					// Retain the value of the previous submission
-					defaultValue={lastResult?.initialValue?.passwordConfirm as string}
-				/>
-				<div>{fields.passwordConfirm.errors}</div>
-			</div>
-			<button
-				type="submit"
-			>{t("pages.reset-password.submit")}</button>
-			{/* Password Reset Token */}
-			<input name="token" type="hidden" value={token} />
-		</form>
+					<div>
+						<Label htmlFor={fields.passwordConfirm.name}>{t("fields.new-password-confirm")}</Label>
+						<Input
+							id={fields.passwordConfirm.name}
+							type="password"
+							key={fields.passwordConfirm.key}
+							name={fields.passwordConfirm.name}
+							// Retain the value of the previous submission
+							defaultValue={lastResult?.initialValue?.passwordConfirm as string}
+						/>
+						<Label htmlFor={fields.passwordConfirm.name} className="font-normal text-red-500">{fields.passwordConfirm.errors}</Label>
+					</div>
+					<Button
+						type="submit"
+						color="sky"
+						className={clsx(
+							"w-full",
+						)}
+					>{t("pages.reset-password.submit")}</Button>
+					{/* Password Reset Token */}
+					<input name="token" type="hidden" value={token} />
+				</Fieldset>
+			</form>
+		</AuthBody>
 	</>
 }
