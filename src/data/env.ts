88dbd413d0429efb49043df965239399b5817673
Enzo <enzo@cheerupemokid.com>
2026-07-29 09:21:52 -0700

    add adminemail to env

--- src/data/env.ts
+++ src/data/env.ts
@@ -1,6 +1,7 @@
 // Environment Variables
 export const directusURL = process.env.NEXT_PUBLIC_CMS_URL!
 export const adminToken = process.env.CMS_ADMIN_TOKEN!
+export const adminEmail = process.env.CMS_ADMIN_EMAIL!
 export const sessionSecret = process.env.CMS_SESSION_SECRET!
 
 export const userCookieName = "dungeoncomic_access_token"
