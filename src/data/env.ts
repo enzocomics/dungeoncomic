56f17697d77dd853a34a81b73fca94e6a678e81b
Enzo <enzo@cheerupemokid.com>
2026-07-29 10:22:57 -0700

    add public admin contact env

--- src/data/env.ts
+++ src/data/env.ts
@@ -3,5 +3,6 @@ export const directusURL = process.env.NEXT_PUBLIC_CMS_URL!
 export const adminToken = process.env.CMS_ADMIN_TOKEN!
 export const adminEmail = process.env.CMS_ADMIN_EMAIL!
 export const sessionSecret = process.env.CMS_SESSION_SECRET!
-
 export const userCookieName = "dungeoncomic_access_token"
+
+export const adminContactEmail = process.env.NEXT_PUBLIC_ADMIN_CONTACT_EMAIL!
