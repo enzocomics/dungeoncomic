30503ea79b0ab7c8eee97d93b77ee9db01be23fd
Enzo <enzo@cheerupemokid.com>
2026-08-04 08:20:52 -0700

    correct email allow list & vars

--- src/data/env.ts
+++ src/data/env.ts
@@ -1,6 +1,9 @@
 // Environment Variables
-export const siteURL = process.env.NEXT_PUBLIC_SITE_URL!
 export const directusURL = process.env.NEXT_PUBLIC_CMS_URL!
+export const siteURL = process.env.NEXT_PUBLIC_SITE_URL!
+export const registerVerifyURL = process.env.NEXT_PUBLIC_REGISTER_VERIFY_URL!
+export const passwordResetURL = process.env.NEXT_PUBLIC_PASSWORD_RESET_URL
+
 export const adminToken = process.env.CMS_ADMIN_TOKEN!
 export const adminEmail = process.env.CMS_ADMIN_EMAIL!
 export const sessionSecret = process.env.CMS_SESSION_SECRET!
