b917ae6fdbda280a245c8d08a474936580a29246
Enzo <enzo@cheerupemokid.com>
2026-08-05 11:13:13 -0700

    add color to defaults

--- scripts/set-defaults.sh
+++ scripts/set-defaults.sh
@@ -5,7 +5,7 @@ source ./.env
 set +a
 
 COLLECTION="settings"
-PAYLOAD='{"project_name": "Dungeon Construction Co.", "project_url": "'"${NEXT_PUBLIC_CMS_URL}"'", "public_registration": "true", "public_registration_role": "5ad2a6f5-74a6-4ebc-9864-5e7b451203d2"  }'
+PAYLOAD=`{"project_name": "Dungeon Construction Co.", "project_url": "'"${NEXT_PUBLIC_CMS_URL}"'", "project_color": "#FFA439", "public_registration": "true", "public_registration_role": "5ad2a6f5-74a6-4ebc-9864-5e7b451203d2"  }`
 
 # Update our project `settings` collection 
 curl -X PATCH "$NEXT_PUBLIC_CMS_URL/items/$COLLECTION" \
