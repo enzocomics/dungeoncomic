e99480cef50791088cce9cf192a108bfd483b026
Enzo <enzo@cheerupemokid.com>
2026-08-10 17:15:23 -0700

    only show comic if it exists in db

--- src/app/(main)/[comic_slug]/layout.tsx
+++ src/app/(main)/[comic_slug]/layout.tsx
@@ -1,7 +1,9 @@
 "use server"
+import { getComic } from "@/lib/directus/get-comics"
 /**----------------------------------- */
 // FUNCTIONS
 import clsx from "clsx"
+import { notFound } from "next/navigation"
 
 /**-----------------------------------
  * COMIC ROUTE LAYOUT
@@ -13,10 +15,17 @@ export default async function ComicLayout({
 	params: Promise<{ comic_slug: string }>
 	children: React.ReactNode
 }) {
+	// Get the comic slug parameter
 	const { comic_slug } = await params
-	return <>
-		Comic Layout<br />
-		get the `comic_slug` in layout: {comic_slug} <br />
-		{children}
-	</>
+	// Check if the comic exists in the database in the db
+	const comic = await getComic(comic_slug)
+
+	// Return the UI only if a comic exists
+	if (!comic) notFound()
+	else
+		return <>
+			Comic Layout<br />
+			get the `comic_slug` in layout: {comic_slug} <br />
+			{children}
+		</>
 }
