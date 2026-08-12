bc9e068fe897fc1bbb586eaa2e96e08ee96efe19
Enzo <enzo@cheerupemokid.com>
2026-08-12 09:41:35 -0700

    check if comicpage exists

--- src/app/(main)/[comic_slug]/[page_num]/page.tsx
+++ src/app/(main)/[comic_slug]/[page_num]/page.tsx
@@ -4,6 +4,8 @@
 import { Metadata } from "next"
 import { getTranslations } from "next-intl/server"
 import ComicPageUI from "./_ui"
+import { getComicPage } from "@/lib/directus/get-comics"
+import { notFound } from "next/navigation"
 
 /**----------------------------------- */
 export default async function ComicPage({
@@ -11,9 +13,16 @@ export default async function ComicPage({
 }: {
 	params: Promise<{ comic_slug: string, page_num: number }>
 }) {
-	return <>
-		<ComicPageUI params={await params} />
-	</>
+	const { comic_slug, page_num } = await params
+	// Get the page details
+	const page = await getComicPage(comic_slug, page_num)
+
+	// 404 if it does not exist
+	if (!page) notFound()
+	else
+		return <>
+			<ComicPageUI params={await params} page={page} />
+		</>
 }
 /** ------------------------------------------------ **
  * Page Metadata
