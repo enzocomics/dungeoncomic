1bcf8a327a35085afddbfc1011378115eb030858
Enzo <enzo@cheerupemokid.com>
2026-08-12 17:53:36 -0700

    cleanup

--- src/app/(main)/[comic_slug]/page.tsx
+++ src/app/(main)/[comic_slug]/page.tsx
@@ -1,13 +1,7 @@
 "use server"
 /**----------------------------------- */
-// TYPES
-import { Metadata } from "next"
-// I18N
-import { getTranslations } from "next-intl/server"
-// DATA
 // UI
 import ComicLandingPageUI from "./_page-ui"
-import { getComic } from "@/lib/directus/get-comics"
 
 /** ------------------------------------------------ **
  *  COMIC ROUTE PAGE
