214b32df778d5e153c940b8910e648ac1b216883
Enzo <enzo@cheerupemokid.com>
2026-08-12 17:39:13 -0700

    get comic details for metadata

--- src/app/(main)/[comic_slug]/page.tsx
+++ src/app/(main)/[comic_slug]/page.tsx
@@ -1,13 +1,38 @@
-"use client"
-
-import clsx from "clsx"
-import ComicLandingPageUI from "./_page-ui"
-
+"use server"
 /**----------------------------------- */
+// TYPES
+import { Metadata } from "next"
+// I18N
+import { getTranslations } from "next-intl/server"
+// DATA
+// UI
+import ComicLandingPageUI from "./_page-ui"
+import { getComic } from "@/lib/directus/get-comics"
 
 /** ------------------------------------------------ **
- * 
+ *  COMIC ROUTE PAGE
  */
-export default function ComicLandingPage() {
+export default async function ComicLandingPage() {
+	// Get the comic slug parameter
 	return <ComicLandingPageUI />
+}
+
+/** ------------------------------------------------ **
+ * Page Metadata
+ * - Will override the global site metadata
+ * - Can use the same page parameters
+ ** ------------------------------------------------ **/
+export async function generateMetadata({
+	params
+}: {
+	params: Promise<{ comic_slug: string }>
+}): Promise<Metadata> {
+	// Get the comic slug parameter
+	const { comic_slug } = await params
+	// Get the comic collection from the CMS
+	const comic = await getComic(comic_slug)
+	return {
+		title: comic.title,
+		description: comic.description
+	}
 }
