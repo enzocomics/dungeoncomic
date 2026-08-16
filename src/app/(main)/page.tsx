1258cf99b6c72b1d436615fdf2a5be73cade655d
Enzo <enzo@cheerupemokid.com>
2026-08-15 19:32:01 -0700

    conditionally render homepage page ui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,11 +3,23 @@
 // LIBRARIES
 import { Metadata } from "next"
 import { getTranslations } from "next-intl/server"
+// DATA
+import { getSettings } from "@/lib/directus/get-settings"
+// UI
 import HomepagePageUI from "./_page-ui"
+import ComicLandingPageUI from "./(comic-route)/[comic_slug]/_page-ui"
 
 /**----------------------------------- */
 export default async function HomePage() {
-	return <HomepagePageUI />
+	// Conditionally render UI based on the frontpage_comic setting
+	const settings = await getSettings()
+	// If a `frontpage_comic` has been set, render comic UI
+	if (settings.frontpage_comic) {
+		return <ComicLandingPageUI />
+	} else {
+		// Otherwise, render the regular homepage UI
+		return <HomepagePageUI />
+	}
 }
 /** ------------------------------------------------ **
  * Page Metadata
