85f32b8361284d7b67c4addcdfd46b63010d67a2
Enzo <enzo@cheerupemokid.com>
2026-08-17 19:18:29 -0700

    comment clarity

--- src/app/(comic)/[route]/[pagenum]/page.tsx
+++ src/app/(comic)/[route]/[pagenum]/page.tsx
@@ -1,7 +1,7 @@
 import { getSettings } from "@/lib/directus/get-settings";
 import ComicPageUI from "../../_page-ui";
 import { notFound } from "next/navigation";
-import { getComic, getComicPage } from "@/lib/directus/get-comics";
+import { getComicPage } from "@/lib/directus/get-comics";
 
 export default async function ComicPagenumPage({
 	params
@@ -16,17 +16,17 @@ export default async function ComicPagenumPage({
 
 	/**----------------------------------- */
 	// LAYOUT MODE 1: IF `frontpage_comic` EXISTS
-	// - Throw a 404
+	// - Throw 404
 	if (frontpage_comic) notFound()
 
 	/**----------------------------------- */
 	// LAYOUT MODE 2
+	// Throw 404 if the pagenum param is not a number
+	if (isNaN(pagenum)) notFound()
 	// Get the comic page
 	const comicPage = await getComicPage(route, pagenum)
-
 	// 404 if it doesn't exist
 	if (!comicPage) notFound()
-
 	// Render
 	return <ComicPageUI params={{ comic_slug: route, page_num: pagenum }} />
 }
