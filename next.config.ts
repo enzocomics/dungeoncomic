10332c42e9f6a133c37cc4a426aecf72b5768771
Enzo <enzo@cheerupemokid.com>
2026-08-18 20:09:39 -0700

    add cms to allowed image hosts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,28 @@
 /**----------------------------------- */
 // LIBRARIES
 import createNextIntlPlugin from "next-intl/plugin"
-
 // TYPES
 import type { NextConfig } from "next"
+import { RemotePattern } from "next/dist/shared/lib/image-config"
+// VARS - URL
+import { directusURL } from "@/data/env"
+const url = new URL(directusURL)
 
 /**----------------------------------- */
 // NEXT.JS CONFIGURATION
 const nextConfig: NextConfig = {
 	output: "standalone",
 	staticPageGenerationTimeout: 1000,
+	images: {
+		remotePatterns: [
+			{
+				protocol: url.protocol.replace(":", ""),
+				hostname: url.hostname,
+				port: url.port,
+				pathname: "/**",
+			} as RemotePattern,
+		],
+	},
 }
 
 /**----------------------------------- */
