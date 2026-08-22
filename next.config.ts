838b12907756613593f1f8747fc19abf60fe91b3
Enzo <enzo@cheerupemokid.com>
2026-08-21 18:14:39 -0700

    allow local ip for image optimziation

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,17 @@ const url = new URL(directusURL)
 // NEXT.JS CONFIGURATION
 const nextConfig: NextConfig = {
 	output: "standalone",
+
 	staticPageGenerationTimeout: 1000,
 	images: {
+		// We are setting this to `true` since the CMS is on the same docker network and thus considered "local"
+		dangerouslyAllowLocalIP: true,
 		remotePatterns: [
 			{
 				protocol: url.protocol.replace(":", ""),
 				hostname: url.hostname,
 				port: url.port,
-				pathname: "/**",
+				pathname: "/assets/**",
 			} as RemotePattern,
 		],
 	},
