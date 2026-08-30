aece49e309d794f50bccf9b4331d912b47a83cda
Enzo <enzo@cheerupemokid.com>
2026-08-29 20:15:53 -0700

    relocate actions

--- src/app/(comic)/[route]/page.tsx
+++ src/app/(comic)/[route]/page.tsx
@@ -11,7 +11,7 @@ import { getComic, getComicPage, getComicVariables } from "@/lib/directus/get-co
 // UI
 import ComicPageUI, { ComicLandingPageUI } from "../_ui/comic-page"
 import { comicMetadata, comicPageMetadata, notFoundMetadata } from "../_ui/metadata"
-import { getUserVarsCookie } from "../actions"
+import { getUserVarsCookie } from "../_functions/actions"
 
 /**-----------------------------------
  * COMIC ROUTE **OR** SUBPAGE
