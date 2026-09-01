3c6543affce93e270e9a19ad80118fa6ed14f680
Enzo <enzo@cheerupemokid.com>
2026-09-01 16:35:42 -0700

    move nav menu to own function

--- src/app/(comic)/_ui/layout.tsx
+++ src/app/(comic)/_ui/layout.tsx
@@ -69,136 +69,7 @@ export function ComicLayoutUI({
 			className={clsx(
 				"font-copy"
 			)}>
-			<Disclosure
-				as="nav"
-				className={clsx(
-					// Structure
-					"fixed!",
-					"z-50",
-					"top-0",
-					"relative",
-					// Size
-					"min-w-xs",
-					"w-full",
-					// Functionality
-					"pointer-events-none",
-				)}
-			>
-				<div className="mx-auto max-w-7xl ">
-					<div className="relative flex items-center justify-between">
-						<div className={clsx(
-							"relative",
-							"inset-y-0",
-							"left-0",
-							"flex",
-							"items-center",
-							"h-12"
-						)}>
-							{/* Mobile menu button*/}
-							<DisclosureButton className={clsx(
-								"group",
-								"flex",
-								"items-center",
-								// Structure
-								"absolute",
-								"-top-0.5",
-								"-left-2",
-								// Appearance
-								"text-comic-accent-500",
-								// Functionality
-								"cursor-pointer",
-								"pointer-events-auto",
-
-							)}>
-								{/* LOGO */}
-								<Seal
-									menu={true}
-									className={clsx(
-										"group/menu",
-										"w-17",
-									)} />
-							</DisclosureButton>
-						</div>
-						<div className={clsx(
-							"flex",
-							"flex-1",
-							// "items-center",
-							// "justify-center",
-							// "sm:items-stretch",
-							// "sm:justify-start"
-						)}>
-
-						</div>
-						<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 pointer-events-auto">
-
-							{/* Profile dropdown */}
-							<Menu as="div" className="relative ml-3">
-								<MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-									<span className="absolute -inset-1.5" />
-									<span className="sr-only">Open user menu</span>
-									<img
-										alt=""
-										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										className="size-12 border-4 border-comic-accent-900 relative rounded-full bg-gray-800"
-									/>
-								</MenuButton>
-
-								<MenuItems
-									transition
-									className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
-								>
-									<MenuItem>
-										<a
-											href="#"
-											className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
-										>
-											Your profile
-										</a>
-									</MenuItem>
-									<MenuItem>
-										<a
-											href="#"
-											className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
-										>
-											Settings
-										</a>
-									</MenuItem>
-									<MenuItem>
-										<a
-											href="#"
-											className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
-										>
-											Sign out
-										</a>
-									</MenuItem>
-								</MenuItems>
-							</Menu>
-						</div>
-					</div>
-				</div>
-
-				{/* PLATFORM MENU */}
-				<DisclosurePanel className="">
-					<div className="space-y-1 px-2 pt-2 pb-3">
-						{navigation.map((item) => (
-							<DisclosureButton
-								key={item.name}
-								as="a"
-								href={item.href}
-								aria-current={item.current ? 'page' : undefined}
-								className={clsx(
-									item.current
-										? 'bg-gray-900 text-white dark:bg-gray-950/50'
-										: 'text-gray-300 hover:bg-white/5 hover:text-white',
-									'block rounded-md px-3 py-2 text-base font-medium',
-								)}
-							>
-								{item.name}
-							</DisclosureButton>
-						))}
-					</div>
-				</DisclosurePanel>
-			</Disclosure>
+			<NavMenu menu={true} />
 
 			{children}
 
@@ -219,6 +90,164 @@ export function FrontpageLayoutUI({
 	children: React.ReactNode
 }) {
 
-	return children
+	return <>
+		<NavMenu />
+		{children}
+	</>
 
+}
+
+
+/**-----------------------------------
+ * FRONTPAGE LAYOUT
+ * ---
+ * - 
+ * 
+ */
+function NavMenu({
+	menu = false,
+}: {
+	menu?: boolean
+}) {
+
+
+	const navigation = [
+		{ name: 'Dashboard', href: '#', current: true },
+		{ name: 'Team', href: '#', current: false },
+		{ name: 'Projects', href: '#', current: false },
+		{ name: 'Calendar', href: '#', current: false },
+	]
+
+	return <>
+		<Disclosure
+			as="nav"
+			className={clsx(
+				// Structure
+				"fixed!",
+				"z-50",
+				"top-0",
+				"relative",
+				// Size
+				"min-w-xs",
+				"w-full",
+				// Functionality
+				"pointer-events-none",
+			)}
+		>
+			<div className="mx-auto max-w-7xl ">
+				<div className="relative flex items-center justify-between">
+					<div className={clsx(
+						"relative",
+						"inset-y-0",
+						"left-0",
+						"flex",
+						"items-center",
+						"h-12"
+					)}>
+						{/* Mobile menu button*/}
+						<DisclosureButton className={clsx(
+							"group",
+							"flex",
+							"items-center",
+							// Structure
+							"absolute",
+							"-top-0.5",
+							"-left-2",
+							// Appearance
+							"text-comic-accent-500",
+							// Functionality
+							"cursor-pointer",
+							"pointer-events-auto",
+
+						)}>
+							{/* LOGO */}
+							<Seal
+								menu={menu}
+								className={clsx(
+									"group/menu",
+									"w-17",
+								)} />
+						</DisclosureButton>
+					</div>
+					<div className={clsx(
+						"flex",
+						"flex-1",
+						// "items-center",
+						// "justify-center",
+						// "sm:items-stretch",
+						// "sm:justify-start"
+					)}>
+
+					</div>
+					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 pointer-events-auto">
+
+						{/* Profile dropdown */}
+						<Menu as="div" className="relative ml-3">
+							<MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+								<span className="absolute -inset-1.5" />
+								<span className="sr-only">Open user menu</span>
+								<img
+									alt=""
+									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+									className="size-12 border-4 border-comic-accent-900 relative rounded-full bg-gray-800"
+								/>
+							</MenuButton>
+
+							<MenuItems
+								transition
+								className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+							>
+								<MenuItem>
+									<a
+										href="#"
+										className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+									>
+										Your profile
+									</a>
+								</MenuItem>
+								<MenuItem>
+									<a
+										href="#"
+										className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+									>
+										Settings
+									</a>
+								</MenuItem>
+								<MenuItem>
+									<a
+										href="#"
+										className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+									>
+										Sign out
+									</a>
+								</MenuItem>
+							</MenuItems>
+						</Menu>
+					</div>
+				</div>
+			</div>
+
+			{/* PLATFORM MENU */}
+			<DisclosurePanel className="">
+				<div className="space-y-1 px-2 pt-2 pb-3">
+					{navigation.map((item) => (
+						<DisclosureButton
+							key={item.name}
+							as="a"
+							href={item.href}
+							aria-current={item.current ? 'page' : undefined}
+							className={clsx(
+								item.current
+									? 'bg-gray-900 text-white dark:bg-gray-950/50'
+									: 'text-gray-300 hover:bg-white/5 hover:text-white',
+								'block rounded-md px-3 py-2 text-base font-medium',
+							)}
+						>
+							{item.name}
+						</DisclosureButton>
+					))}
+				</div>
+			</DisclosurePanel>
+		</Disclosure>
+	</>
 }
