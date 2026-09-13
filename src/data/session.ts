ca76f4dfc71dff562886ae7075b7e13f84064931
enzocomics <enzo@cheerupemokid.com>
2026-09-12 17:06:33 -0700

    add avatar to verifySession object

--- src/data/session.ts
+++ src/data/session.ts
@@ -3,7 +3,13 @@ import "server-only"
 // LIBRARIES
 import { cache } from "react"
 // CMS
-import { readMe, readRole, readUser, readUserPermissions } from "@directus/sdk"
+import {
+	readFile,
+	readMe,
+	readRole,
+	readUser,
+	readUserPermissions,
+} from "@directus/sdk"
 import { adminClient, userClient } from "@/lib/directus/clients"
 
 /** ------------------------------------------------ **
@@ -27,14 +33,30 @@ export const verifySession = cache(async () => {
 				],
 			}),
 		)
-
 		// Get the logged-in user permissions
 		const readMyPermissions = await userClient.request(readUserPermissions())
+
+		// Get the logged-in user avatar
+		const readMyAvatar =
+			readMeResponse && readMeResponse.avatar
+				? await userClient.request(
+						readFile(String(readMeResponse.avatar), {
+							fields: [
+								"filename_disk",
+								"height",
+								"width",
+								"description",
+								"type",
+							],
+						}),
+					)
+				: null
 
 		// Merge them into the same object
 		const mergedObject = {
 			...readMeResponse,
 			permissions: readMyPermissions,
+			avatar: readMyAvatar,
 		}
 
 		return mergedObject
