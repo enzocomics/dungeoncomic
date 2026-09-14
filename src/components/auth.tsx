6fe3412632f4a07a054f48117e1bee301efc2a4e
Enzo <enzo@cheerupemokid.com>
2026-09-14 12:43:54 -0700

    reusable authlink component

---
+++ src/components/auth.tsx
@@ -0,0 +1,37 @@
+
+import Link from "next/link"
+import { ComponentPropsWithoutRef } from "react"
+import { AuthModalSchema, useGlobalContext } from "@/app/_context"
+
+/** ---
+ * A reusable link to the login/register/reset-password pages.
+ * - If a modal is currently active, it navigates within the modal
+ * - If the modal is not currently active, it opens it up
+ *
+ */
+export function AuthLink({
+	modal,
+	href = `/${modal}`,
+	...props
+}: Omit<ComponentPropsWithoutRef<typeof Link>, "href"> & {
+	href?: string
+	modal: AuthModalSchema
+}) {
+	// Get the authModal context
+	const { authModal, setOpenAuthModal } = useGlobalContext()
+
+	// Render
+	return <Link
+		{...props}
+		href={href}
+		onClick={(e) => {
+			if (authModal) {
+				e.preventDefault()
+				setOpenAuthModal(modal)
+			}
+			props.onClick?.(e)
+		}}
+	>
+		{props.children}
+	</Link>
+}
