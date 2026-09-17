da3211c2c08b2a7580359a8f2e51fb880ce59d51
Enzo <enzo@cheerupemokid.com>
2026-09-17 12:02:00 -0700

    helper functions for comic page booleans

---
+++ src/app/(comic)/_functions/check-pages.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { getComic, getComicPage } from "@/lib/directus/get-comics"
+
+/**----------------------------------- */
+export const checkCoverPage = (
+	landingPage: Awaited<ReturnType<typeof getComic>>["landing_page"]
+) => {
+	return !!(landingPage === "cover-page")
+}
+
+export const checkHasPrevPage = (
+	prevPages: Awaited<ReturnType<typeof getComicPage>>["prev_pages"]
+) => {
+	return !!(
+		prevPages &&
+		prevPages.length > 0 &&
+		prevPages.some(
+			// checks that at least ONE page is published
+			p => p.pages_id.status === "published"
+		)
+	)
+}
+
+export const checkHasNextPage = (
+	nextPages: Awaited<ReturnType<typeof getComicPage>>["next_pages"]
+) => {
+	return !!(
+		nextPages &&
+		nextPages.length > 0 &&
+		nextPages.some(
+			// checks that at least ONE page is published
+			p => p.linked_pages_id.status === "published"
+		)
+	)
+}
