c0841cce61878baa616636742e7e9b3826f65aab
Enzo <enzo@cheerupemokid.com>
2026-09-18 16:53:39 -0700

    add limit to getcomic fetch

--- src/lib/directus/get-comics.ts
+++ src/lib/directus/get-comics.ts
@@ -6,67 +6,65 @@ import { readItems } from "@directus/sdk"
 import { cache } from "react"
 
 /** ------------------------------------------------ **
- * GET COMICS
- */
-
-/** ------------------------------------------------ **
  * GET COMIC
  */
-export const getComic = cache(async ({ slug }: { slug?: string }) => {
-	const request = await publicClient.request(
-		readItems("comics", {
-			filter: slug
-				? {
-						slug: { _eq: slug },
-					}
-				: undefined,
-			limit: 1,
-			fields: [
-				// Details
-				"title",
-				"description",
-				"slug",
-				{
-					authors: [
-						"id",
-						"email",
-						"name",
-						"username",
-						"avatar",
-						"homepage_url",
-					],
-				},
-				// Appearance
-				{
-					logo: ["filename_disk", "type", "width", "height", "description"],
-				},
-				{
-					thumbnail: [
-						"filename_disk",
-						"type",
-						"width",
-						"height",
-						"description",
-					],
-				},
-				{
-					banner: ["filename_disk", "type", "width", "height", "description"],
-				},
-				"accent_color",
-				"display_font",
-				"copy_font",
-				// Content
-				"landing_page",
-				"landing_page_content",
-				"start_button_text",
-				// Meta
-				"id",
-				"count(pages)",
-			],
-		}),
-	)
-	return request?.[0]
-})
+export const getComic = cache(
+	async ({ slug, limit = 1 }: { slug?: string; limit?: number }) => {
+		const request = await publicClient.request(
+			readItems("comics", {
+				filter: slug
+					? {
+							slug: { _eq: slug },
+						}
+					: undefined,
+				limit: limit,
+				fields: [
+					// Details
+					"title",
+					"description",
+					"slug",
+					{
+						authors: [
+							"id",
+							"email",
+							"name",
+							"username",
+							"avatar",
+							"homepage_url",
+						],
+					},
+					// Appearance
+					{
+						logo: ["filename_disk", "type", "width", "height", "description"],
+					},
+					{
+						thumbnail: [
+							"filename_disk",
+							"type",
+							"width",
+							"height",
+							"description",
+						],
+					},
+					{
+						banner: ["filename_disk", "type", "width", "height", "description"],
+					},
+					"accent_color",
+					"display_font",
+					"copy_font",
+					// Content
+					"landing_page",
+					"landing_page_content",
+					"start_button_text",
+					// Meta
+					"id",
+					"count(pages)",
+				],
+			}),
+		)
+		return request?.[0]
+	},
+)
 
 /** ------------------------------------------------ **
  * GET SINGLE COMIC PAGE
