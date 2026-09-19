c084b55330cd760fdad79dadb77b0ecb14dd31b8
Enzo <enzo@cheerupemokid.com>
2026-09-18 23:08:42 -0700

    div prevents link from growing too big

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -40,71 +40,73 @@ export default async function PlatformLayout(props: LayoutProps<"/">) {
 				"justify-center",
 				"items-center",
 			)}>
-				<Link href="/" className={clsx(
-					"w-full",
-					"flex",
-					"items-center",
-					"group",
-					"cursor-pointer",
-					"h-full",
-					"hover:scale-105",
-					"hover:duration-0",
-					// Transition
-					"transition-all",
-					"ease-in-out",
-					"duration-300",
-					"rounded",
-					"data-open:bg-comic-accent-700/80",
-					"data-open:outline-4",
-					"data-open:outline-comic-accent-500",
-					"data-open:outline-offset-2",
-					"flex",
-					"justify-center",
-					"items-center",
-				)}>
-					{!logo &&
-						<span className={clsx(
-							"inline-block",
-							"font-platform-display",
-							"text-xl",
-							"md:text-4xl",
-							"text-center",
-							"text-pretty",
-							"font-bold",
-							"p-4",
-							"w-full",
-							"whitespace-nowrap",
-							"overflow-x-hidden",
-							"text-ellipsis",
-							!!banner && [
-								"text-white",
-								"[text-stroke:8px_black",
-								"[-webkit-text-stroke:8px_black]",
-								"[paint-order:stroke_fill]",
-								"drop-shadow-black/50",
-								"drop-shadow-md",
-							],
+				<div>
+					<Link href="/" className={clsx(
+						"w-full",
+						"flex",
+						"items-center",
+						"group",
+						"cursor-pointer",
+						"h-full",
+						"hover:scale-105",
+						"hover:duration-0",
+						// Transition
+						"transition-all",
+						"ease-in-out",
+						"duration-300",
+						"rounded",
+						"data-open:bg-comic-accent-700/80",
+						"data-open:outline-4",
+						"data-open:outline-comic-accent-500",
+						"data-open:outline-offset-2",
+						"flex",
+						"justify-center",
+						"items-center",
+					)}>
+						{!logo &&
+							<span className={clsx(
+								"inline-block",
+								"font-platform-display",
+								"text-xl",
+								"md:text-4xl",
+								"text-center",
+								"text-pretty",
+								"font-bold",
+								"p-4",
+								"w-full",
+								"whitespace-nowrap",
+								"overflow-x-hidden",
+								"text-ellipsis",
+								!!banner && [
+									"text-white",
+									"[text-stroke:8px_black",
+									"[-webkit-text-stroke:8px_black]",
+									"[paint-order:stroke_fill]",
+									"drop-shadow-black/50",
+									"drop-shadow-md",
+								],
 
-						)}>
-							{settings.project_name}
-						</span>
-					}
-					{logo &&
-						<Image
-							src={`${directusURL}/assets/${logo.filename_disk}`}
-							alt={logo.description || ""}
-							width={logo.width || "160"}
-							height={logo.height || "120"}
-							className={clsx(
-								"drop-shadow-black/50",
-								"drop-shadow-sm",
-								"w-auto",
-								"max-h-8",
-								"md:max-h-15",
-							)}
-						/>
-					}
-				</Link>
+							)}>
+								{settings.project_name}
+							</span>
+						}
+						{logo &&
+							<Image
+								src={`${directusURL}/assets/${logo.filename_disk}`}
+								alt={logo.description || ""}
+								width={logo.width || "160"}
+								height={logo.height || "120"}
+								className={clsx(
+									"drop-shadow-black/50",
+									"drop-shadow-sm",
+									"w-auto",
+									"max-h-8",
+									"md:max-h-15",
+								)}
+							/>
+						}
+					</Link>
+				</div>
 			</header>
 			<div className={clsx(
 				"relative",
