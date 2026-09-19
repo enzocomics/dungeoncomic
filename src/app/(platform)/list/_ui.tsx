9f6d024adabe553c6c0c5869baf8ebf3f7450af7
Enzo <enzo@cheerupemokid.com>
2026-09-18 19:44:53 -0700

    create ui page

---
+++ src/app/(platform)/list/_ui.tsx
@@ -0,0 +1,9 @@
+
+
+export const ListPageUI = (
+
+) => (
+	<>
+		List Page
+	</>
+)
--- src/app/(platform)/list/page.tsx
+++ src/app/(platform)/list/page.tsx
@@ -1,12 +1,19 @@
+import { LandingPageBody, LandingPageContent } from "@/app/_ui/page-landing"
 import { getSettings } from "@/lib/directus/get-settings"
 import { notFound } from "next/navigation"
+import { ListPageUI } from "./_ui"
+import { getComic, getComics } from "@/lib/directus/get-comics"
 
 export default async function List() {
 	const settings = await getSettings()
 	const singleComic = !!settings.single_comic_site
 
+	// Don't show this route on single-comic sites
 	if (singleComic) notFound()
+
+	const comics = await getComics()
+
 	return <>
-		hi
+		<ListPageUI />
 	</>
 }
