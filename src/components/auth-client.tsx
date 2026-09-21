056bbdb030fc60a94a7e0344e2c6424e76cd607a
Enzo <enzo@cheerupemokid.com>
2026-09-20 20:56:59 -0700

    move logout button to component

---
+++ src/components/auth-client.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Link from "next/link"
+import { ComponentPropsWithoutRef } from "react"
+import { AuthModalSchema, useGlobalContext } from "@/app/_context"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+
+export function LogoutButton({
+	...props
+}: ComponentPropsWithoutRef<"button">) {
+	const router = useRouter()
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+
+	function handleLogout() {
+		const queryString = searchParams.toString()
+
+		const referralPath = queryString
+			? `${pathname}?${queryString}`
+			: pathname
+
+		const logoutParams = new URLSearchParams({
+			r: referralPath
+		})
+
+		router.push(`/logout?${logoutParams.toString()}`)
+	}
+
+	return <button
+		{...props}
+		onClick={handleLogout}
+	>
+		{props.children}
+	</button>
+}
