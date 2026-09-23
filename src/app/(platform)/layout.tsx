8d40fa3a26fd0922fa21fe590671fddc2acfeae8
Enzo <enzo@cheerupemokid.com>
2026-09-22 21:19:33 -0700

    platform page ui

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -13,6 +13,10 @@ import Link from "next/link"
 import { PageContentWrapper } from "../_ui/site-page"
 import { verifySession } from "@/data/session"
 import StatusMessage from "@/components/status-message"
+import SiteNav from "../_ui/site-nav"
+import { SiteLayoutMain } from "../_ui/site-layout"
+import SiteFooter from "../_ui/site-footer"
+import ClientLandingPageHeader from "../(comic)/[route]/@header/_effects"
 
 export default async function PlatformLayout(props: LayoutProps<"/">) {
 	const { public_registration } = await adminClient.request(readSettings({
@@ -26,101 +30,90 @@ export default async function PlatformLayout(props: LayoutProps<"/">) {
 	return <>
 		<AuthModal public_registration={public_registration} />
 		<PlatformLayoutUI session={session}>
-			<header className={clsx(
-				// Structure
-				"fixed",
-				"z-10",
-				"left-0",
-				"md:left-1/2",
-				"md:-translate-x-1/2",
-				// Size
-				"w-full",
-				"min-w-xs",
-				"max-w-6xl",
-				"px-16",
-				"h-12",
-				"md:h-22",
-				"flex",
-				"justify-center",
-				"items-center",
-			)}>
-				<div>
-					<Link href="/" className={clsx(
-						"w-full",
-						"flex",
-						"items-center",
-						"group",
-						"cursor-pointer",
-						"h-full",
-						"hover:scale-105",
-						"hover:duration-0",
-						// Transition
-						"transition-all",
-						"ease-in-out",
-						"duration-300",
-						"rounded",
-						"data-open:bg-comic-accent-700/80",
-						"data-open:outline-4",
-						"data-open:outline-comic-accent-500",
-						"data-open:outline-offset-2",
-						"flex",
-						"justify-center",
-						"items-center",
-					)}>
-						{!logo &&
-							<span className={clsx(
-								"inline-block",
-								"font-platform-display",
-								"text-xl",
-								"md:text-4xl",
-								"text-center",
-								"text-pretty",
-								"font-bold",
-								"p-4",
-								"w-full",
-								"whitespace-nowrap",
-								"overflow-x-hidden",
-								"text-ellipsis",
-								!!banner && [
-									"text-white",
-									"[text-stroke:8px_black",
-									"[-webkit-text-stroke:8px_black]",
-									"[paint-order:stroke_fill]",
-									"drop-shadow-black/50",
-									"drop-shadow-md",
-								],
+			<SiteNav
+				session={session}
+				settings={settings}
+				menu={true}
+			>
+				<Link href="/" className={clsx(
+					"pointer-events-auto",
+					"w-auto",
+					"flex",
+					"items-center",
+					"group",
+					"cursor-pointer",
+					"h-full",
+					"hover:scale-105",
+					"hover:duration-0",
+					// Transition
+					"transition-all",
+					"ease-in-out",
+					"duration-300",
+					"rounded",
+					"data-open:bg-comic-accent-700/80",
+					"data-open:outline-4",
+					"data-open:outline-comic-accent-500",
+					"data-open:outline-offset-2",
+					"flex",
+					"justify-center",
+					"items-center",
+				)}>
+					{!logo &&
+						<span className={clsx(
+							"inline-block",
+							"font-platform-display",
+							"text-xl",
+							"md:text-4xl",
+							"text-center",
+							"text-pretty",
+							"font-bold",
+							// "p-4",
+							"w-full",
+							"whitespace-nowrap",
+							"overflow-x-hidden",
+							"text-ellipsis",
+							!!banner && [
+								"text-white",
+								"[text-stroke:8px_black",
+								"[-webkit-text-stroke:8px_black]",
+								"[paint-order:stroke_fill]",
+								"drop-shadow-black/50",
+								"drop-shadow-md",
+							],
 
-							)}>
-								{settings.project_name}
-							</span>
-						}
-						{logo &&
-							<Image
-								src={`${directusURL}/assets/${logo.filename_disk}`}
-								alt={logo.description || ""}
-								width={logo.width || "160"}
-								height={logo.height || "120"}
-								className={clsx(
-									"drop-shadow-black/50",
-									"drop-shadow-sm",
-									"w-auto",
-									"max-h-8",
-									"md:max-h-15",
-								)}
-							/>
-						}
-					</Link>
-				</div>
-			</header>
-			<div className={clsx(
+						)}>
+							{settings.project_name}
+						</span>
+					}
+					{logo &&
+						<Image
+							src={`${directusURL}/assets/${logo.filename_disk}`}
+							alt={logo.description || ""}
+							width={logo.width || "160"}
+							height={logo.height || "120"}
+							className={clsx(
+								"drop-shadow-black/50",
+								"drop-shadow-sm",
+								"w-auto",
+								"h-14",
+								"md:h-19",
+							)}
+						/>
+					}
+				</Link>
+			</SiteNav>
+			<SiteLayoutMain>
+				{/* <div className={clsx(
 				"relative",
 				"pt-12",
 				"md:pt-22",
-			)}>
+			)}> */}
 				<PageContentWrapper>
 					{props.children}
 				</PageContentWrapper>
-			</div>
+				{/* </div> */}
+			</SiteLayoutMain>
+			<SiteFooter />
 		</PlatformLayoutUI >
 	</>
 }
